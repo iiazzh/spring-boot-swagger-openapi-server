714af12df9082848264b1eae1983ea7f8a2e220e
suhas <86886386+iiazzh@users.noreply.github.com>
2021-10-13 19:46:33 +0530

    First commit. Add basic project files.

---
+++ src/test/java/com/suhasmh/swaggeropenapiserver/SwaggeropenapiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhasmh.swaggeropenapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggeropenapiserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
